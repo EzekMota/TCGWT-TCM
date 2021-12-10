a0960c72d118718b67f98d82ecd312ca30e343fe
Leofoli <Leoliveirafe@protonmail.com>
2021-12-10 13:29:52 -0300

    clientes fixed

---
+++ TCCGWT/Controllers/ClienteController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TCCGWT.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace TCCGWT.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        string baseurl = "http://20.114.208.185";
+        public async Task<ActionResult> Clientes()
+        {
+            List<ClienteModel> CliInfo = new List<ClienteModel>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseurl);
+                client.DefaultRequestHeaders.Clear();
+                HttpResponseMessage Res = await client.GetAsync("api/cliente");
+                if (Res.IsSuccessStatusCode)
+                {
+                    var CliResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    if(CliResponse == null)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    CliInfo = JsonConvert.DeserializeObject<List<ClienteModel>>(CliResponse);
+                }
+                return View(CliInfo);
+            }
+            
+        }
+    }
+}
---
+++ TCCGWT/Models/ClienteModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TCCGWT.Models
+{
+    public class ClienteModel
+    {
+        public int IdCli { get; set; }
+
+        public string NomeCli { get; set; }
+
+        public string EmailCli { get; set; }
+
+        public string UserCli { get; set; }
+
+        public string SenhaCli { get; set; }
+
+        public string TelefoneCli { get; set; }
+
+        public string CepCli { get; set; }
+
+        public string LogradouroCli { get; set; }
+
+        public string NumCli { get; set; }
+
+        public string ComplementoCli { get; set; }
+    }
+}
