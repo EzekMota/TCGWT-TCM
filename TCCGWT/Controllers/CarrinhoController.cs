28edc645d83280bb6fed7495155565a1ebd65903
Leofoli <Leoliveirafe@protonmail.com>
2021-12-13 15:16:01 -0300

    carrinho added and layout fixed

---
+++ TCCGWT/Controllers/CarrinhoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TCCGWT.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+
+namespace TCCGWT.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        // GET: Carrinho
+        string baseurl = "http://20.114.208.185";
+        public async Task<ActionResult> Carrinho()
+        {
+            List<carrinhoModel> CarInfo = new List<carrinhoModel>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseurl);
+                client.DefaultRequestHeaders.Clear();
+                HttpResponseMessage Res = await client.GetAsync("api/carrinho");
+                if (Res.IsSuccessStatusCode)
+                {
+                    var CarResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    if (CarResponse == null)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    CarInfo = JsonConvert.DeserializeObject<List<carrinhoModel>>(CarResponse);
+                }
+                return View(CarInfo);
+            }
+
+        }
+    }
+}
---
+++ TCCGWT/Models/carrinhoModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TCCGWT.Models
+{
+    public class carrinhoModel
+    {
+        public int IdCarrinho { get; set; }
+
+        public string CepCarrinho { get; set; }
+
+        public string LogradouroCarrinho { get; set; }
+
+        public int NumCarrinho { get; set; }
+
+        public string ComplementoCarrinho { get; set; }
+        //fk
+        public int IdPedido { get; set; }
+        //fk
+        public int IdCli { get; set; }
+    }
+}
