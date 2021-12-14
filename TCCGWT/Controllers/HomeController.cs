3a353764adc97ab54ba14c9cde4e4d2671cd2ead
AugustoCarvalho <gutinho102004@gmail.com>
2021-12-14 18:22:24 -0300

    layouts atualizados

--- TCCGWT/Controllers/HomeController.cs
+++ TCCGWT/Controllers/HomeController.cs
@@ -35,5 +35,10 @@ namespace TCCGWT.Controllers
         {
             return View();
         }
+
+        public ActionResult IndexLogadoFunc()
+        {
+            return View();
+        }
     }
 }
