01a67d392eb394bdbc805dca6f91229cca0743b9
Leofoli <Leoliveirafe@protonmail.com>
2021-12-14 03:25:05 -0300

    cadastro funcionando, fixar a máscara quando acordar

---
+++ TCCGWT/Models/ClienteCadastroModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TCCGWT.Models
+{
+    public class ClienteCadastroModel
+    {
+        public string NomeCli { get; set; }
+
+        public string EmailCli { get; set; }
+
+        public string UserCli { get; set; }
+
+        public string SenhaCli { get; set; }
+
+        public string TelefoneCli { get; set; }
+
+        public string CepCli { get; set; }
+
+        public string LogradouroCli { get; set; }
+
+        public string NumCli { get; set; }
+
+        public string ComplementoCli { get; set; }
+    }
+}
