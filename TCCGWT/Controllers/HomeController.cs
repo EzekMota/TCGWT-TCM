e18525e0ca01a6dd176d4061cb15254bce570802
AugustoCarvalho <gutinho102004@gmail.com>
2021-12-14 22:44:19 -0300

    cadastro de reserva bonito

--- TCCGWT/Controllers/HomeController.cs
+++ TCCGWT/Controllers/HomeController.cs
@@ -35,7 +35,12 @@ namespace TCCGWT.Controllers
         {
             return View();
         }
-        
+
+        public ActionResult IndexLogadoMan()
+        {
+            return View();
+        }
+
         public ActionResult Comandas()
         {
             return View();
